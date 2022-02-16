1d40f87c709ba1a94d850d385533c9d3c61ba35f
lechiffre4 <toxcoa@gmail.com>
2022-02-16 16:48:19 -0500

    #6 function to interpret prolarity with a filter

--- function.py
+++ function.py
@@ -6,6 +6,7 @@ import random
 import json
 import geocoder
 from textblob import TextBlob
+import re
 
 
 
@@ -53,15 +54,46 @@ def getTextinTrend(trend_name):
     tweettext = []
     tweets = tweepy.Cursor(api.search_tweets,
               q=trend_name,
-              lang="en").items(100)
+              lang="en").items(250)
 
     for tweet in tweets:
-        tweettext.append(tweet.text)
+        result = re.sub(r"http\S+", "", tweet.text)
+        tweettext.append(result)
 
     tweettext = "".join(tweettext)
     return tweettext
 
-search = "#chocolat"
-blob = TextBlob(getTextinTrend(search))
-sentiment = blob.sentiment.polarity
-print(search+": "+ str(sentiment))
+
+def getSentimentFromHashtags(hashtag):
+    text = getTextinTrend(hashtag)
+    print(text)
+    blob = TextBlob(text)
+    sentiment = blob.sentiment.polarity # value between -1 and 1
+    print(hashtag+": "+ str(sentiment))
+    return sentiment
+
+def interpretPolarity(polarity):
+    sentiment = None
+    if(polarity>=0.6):
+        print("peace")
+    elif(polarity<=0.5 and polarity>0.4):
+        print("happy conversations")
+    elif(polarity<=0.4 and polarity>0.3):
+        print("normal conversation")
+    elif(polarity<=0.3 and polarity>0.2):
+        print("became titled")
+    elif(polarity<=0.2 and polarity>0.1):
+        print("titled")
+    elif(polarity<=0.1 and polarity>0):
+        print("dangerous")
+    elif(polarity<=0 and polarity> -0.1):
+        print("hardcore")
+    elif(polarity<= -0.1 and polarity> -0.4):
+        print("cursed topic")
+    elif(polarity<= -0.4):
+        print("anarchy")
+
+interpretPolarity(getSentimentFromHashtags("#blm"))
+
+
+    
